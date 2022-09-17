5a0194be565025691792d702717c02c6243517fe
CQMian <89206158+CQMian@users.noreply.github.com>
2022-09-17 01:41:07 -0400

    Update Water.js

--- src/Components/Water.js
+++ src/Components/Water.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+
+
 function Water() {
   return (
-    <div>Water</div>
+    <head>
+      <title>3 Water Bar</title>
+      <script type="text/javascript">
+        var bodyText = ["Text here"]
+        {/* function generateText(sentenceCount){
+			    for (var i=0; i<sentenceCount; i++)
+			      document.write(bodyText[Math.floor(Math.random()*7)]+" ");
+		      } */}
+      </script>
+		
+      <div>Water
+      
+        
+      
+
+    </div>
+    </head>  
   )
+
 }
 
 export default Water
